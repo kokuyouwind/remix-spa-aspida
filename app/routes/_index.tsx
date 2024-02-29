66651cbffc1765d8c7175db6583daf03ef0b962e
kokuyouwind <kokuyouwind@gmail.com>
2024-02-29 10:33:38 +0900

    adding @aspida/axios causes build error

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import aspida from "@aspida/axios";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,6 +9,7 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  aspida();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix (SPA Mode)</h1>
